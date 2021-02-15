11f3bd5382afa873935832efff4bec2f75b3d9d8
Chandrahasa Reddy <chandrahasa.reddy@rbc.com>
2021-02-15 12:48:59 -0500

    first commit

---
+++ src/main/java/com/spring/beerworks/beerworks/BeerworksApplication.java
@@ -0,0 +1,13 @@
+package com.spring.beerworks.beerworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerworksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/beerworks/beerworks/BeerworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.beerworks.beerworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerworksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
